658b3b9a612189fb0fffb50632c3b7bc7bc9b3f7
Baratam Manichandana <baratam@jmangroup.com>
2025-02-20 15:53:46 +0530

    done

--- generate/file_generate.py
+++ generate/file_generate.py
@@ -1,19 +1,25 @@
 import random
 import string
 import os
+from datetime import datetime
+import os
+import time
 
-def generate_random_text(length):
-    letters = string.ascii_letters + string.digits
-    return ''.join(random.choice(letters) for _ in range(length))
+def generate_random_text():
+    # letters = string.ascii_letters + string.digits
+    letters=datetime.now().strftime("%H-%M-%S %d-%m-%Y") 
+    return ''.join(letters)
 
 
 def create_new_file():
-    random_name = generate_random_text(10)
-    file_name = os.path.join( "files", f"{random_name}.txt")
+    path='/app/files'
+    file_name=len(os.listdir(path))+1
+    random_name = generate_random_text()
+    file_name = os.path.join( "files", f"{file_name}.txt")
     with open(file_name, "w") as file:
         random_text = random_name
         file.write(random_text)
 
     print(f"New file created: {file_name}")
-
-create_new_file()
+if __name__ =="__main__":
+    create_new_file()
--- show/display.py
+++ show/display.py
@@ -1,11 +1,13 @@
 import os
+import time
 path='/app/files'
-file_name=os.listdir(path)[0]
+file_name=str(len(os.listdir(path)))
+file_name=file_name+".txt"
 filepath = os.path.join(path, file_name)
 def display():
     with open(filepath,"r") as f:
         lines = f.readlines()
         for line in lines:
             print(line.strip())
-if __name__ == "__main__":
-    display()   
+if __name__ == "__main__":  
+    display()
